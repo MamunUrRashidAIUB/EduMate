43a1d2c6f7da6d1f1c935266950bbf737fb7a9eb
MamunUrRashidAIUB <md.mamun.ur.rashid.cse@gmail.com>
2025-08-16 01:12:54 +0600

    added home screen and also login button clickable

--- app/src/main/java/com/example/edumate/LoginActivity.kt
+++ app/src/main/java/com/example/edumate/LoginActivity.kt
@@ -2,6 +2,7 @@ package com.example.edumate
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -15,6 +16,13 @@ class LoginActivity :AppCompatActivity() {
             val intent =Intent(this,SignupActivity::class.java)
             startActivity(intent)
         }
+
+        val homeScreenShow=findViewById<Button>(R.id.loginButton)
+        homeScreenShow.setOnClickListener {
+            val intent=Intent(this,home::class.java)
+            startActivity(intent)
+        }
+
 
     }
 
---
+++ app/src/main/java/com/example/edumate/home.kt
@@ -0,0 +1,14 @@
+package com.example.edumate
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class home :AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.home)
+    }
+
+}
